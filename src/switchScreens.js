57d0ea4209da109cb1e50067f86e8630a8044a49
Sena Aydin <senaaydin484@gmail.com>
2023-11-01 18:24:52 +0100

    switch screens

--- src/switchScreens.js
+++ src/switchScreens.js
@@ -2,7 +2,6 @@ function switchScreens () {
    const button = document.getElementById("start-button")
     document.getElementById("start-screen").style.display = "none";
     document.getElementById("game-screen").style.display = "block"
-    document.querySelector("body").style.backgroundColor = " #ffee93"
-
+    document.querySelector("body").style.backgroundColor = "#ffee93"
 }
 
