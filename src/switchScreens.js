2a2028e2c50ddbfeb9f68db82797c4d2ec318fa2
Sena Aydin <senaaydin484@gmail.com>
2023-11-01 16:49:47 +0100

    rename character to victim, add switch windows functionality

---
+++ src/switchScreens.js
@@ -0,0 +1,8 @@
+function switchScreens () {
+   const button = document.getElementById("start-button")
+    document.getElementById("start-screen").style.display = "none";
+    document.getElementById("game-screen").style.display = "block"
+    document.querySelector("body").style.backgroundColor = " #ffee93"
+
+}
+
