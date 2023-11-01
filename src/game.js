0e62d2539d8be68779720be9b9e6d9e143255b52
Sena Aydin <senaaydin484@gmail.com>
2023-11-01 18:36:17 +0100

    OOP

--- src/game.js
+++ src/game.js
@@ -1,110 +1,112 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const mainVictim = document.querySelector('.main-victim');
-  const stepSize = 100;
-  const windowWidth = window.innerWidth;
+class Game {
+  constructor() {
+    this.mainVictim = null;
+    this.stepSize = 100;
+    this.windowWidth = 0;
+    this.victimX = 0;
+    this.score = 0;
+    this.scoreElement = null;
+    this.redFlagContainer = null;
+    this.greenFlagContainer = null;
+    this.flagSpeed = 5;
 
-  let victimX = (windowWidth - mainVictim.clientWidth) / 2; // Center the victim horizontally
+    // Initialize event listeners
+    document.addEventListener('DOMContentLoaded', this.init.bind(this));
+    document.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
 
-  // Function to update victim position
-  const updateMainVictimPosition = () => {
-    mainVictim.style.left = victimX + 'px';
-  };
+  init() {
+    this.mainVictim = document.querySelector('.main-victim');
+    this.windowWidth = window.innerWidth;
+    this.victimX = (this.windowWidth - this.mainVictim.clientWidth) / 2;
+    this.scoreElement = document.getElementById('score');
+    this.redFlagContainer = document.querySelector('.red-flags');
+    this.greenFlagContainer = document.querySelector('.green-flags');
 
-  // Initialize the score
-  let score = 0;
-  const scoreElement = document.getElementById('score');
+    this.updateMainVictimPosition();
+    this.updateScore();
+    this.createRandomFlags();
+  }
 
-  // Function to update the score on the score board
-  const updateScore = () => {
-    scoreElement.textContent = score;
-  };
+  updateMainVictimPosition() {
+    this.mainVictim.style.left = this.victimX + 'px';
+  }
 
-  // Function to create a new flag and add it to the specified container
-  const createFlag = (container, type) => {
+  updateScore() {
+    this.scoreElement.textContent = this.score;
+    if(this.score < 0) {
+      document.getElementById("game-screen").style.display = "none"
+      document.getElementById("end-screen").style.display = "block";
+    }
+  }
+
+  createFlag(container, type) {
     const flag = document.createElement('div');
     flag.classList.add(`${type}-flag`, 'flag');
-
-    // Set a random position for the flag at the top of the screen
-    const randomX = Math.floor(Math.random() * (windowWidth - 50));
+    const randomX = Math.floor(Math.random() * (this.windowWidth - 50));
     flag.style.left = `${randomX}px`;
     flag.style.top = '0';
-
     container.appendChild(flag);
+    this.moveFlag(flag, type);
+  }
 
-    // Move the flag towards the victim
-    moveFlag(flag, type);
-  };
-
-  // Function to move the flag towards the victim
-  const moveFlag = (flag, type) => {
-    const flagSpeed = 5; 
-
+  moveFlag(flag, type) {
     const flagInterval = setInterval(() => {
       const flagY = parseInt(flag.style.top);
       const flagX = parseInt(flag.style.left);
 
-      // Calculate the bounding boxes for victim and flag
-      const victimRect = mainVictim.getBoundingClientRect();
+      const victimRect = this.mainVictim.getBoundingClientRect();
       const flagRect = flag.getBoundingClientRect();
 
-      // Check for collision between victim and flag
       if (
         victimRect.right > flagRect.left &&
         victimRect.left < flagRect.right &&
         victimRect.bottom > flagRect.top &&
         victimRect.top < flagRect.bottom
       ) {
-        // Collision with a flag
         if (type === 'red') {
-          score -= 10;
+          this.score -= 10;
         } else {
-          score += 10;
+          this.score += 10;
         }
-        updateScore();
+        this.updateScore();
         flag.remove();
         clearInterval(flagInterval);
       }
 
-      // Move the flag vertically towards the victim
-      flag.style.top = flagY + flagSpeed + 'px';
+      flag.style.top = flagY + this.flagSpeed + 'px';
 
-      // Check if the flag is out of bounds and remove it
       if (flagY >= window.innerHeight) {
         flag.remove();
         clearInterval(flagInterval);
       }
     }, 10);
-  };
+  }
 
-  // Initial position and score update
-  updateMainVictimPosition();
-  updateScore();
+  handleKeyDown(event) {
+    if (event.key === "ArrowLeft" && this.victimX > 0) {
+      this.victimX -= this.stepSize;
+    } else if (event.key === "ArrowRight" && this.victimX < this.windowWidth - this.mainVictim.clientWidth) {
+      this.victimX += this.stepSize;
+    }
+    this.updateMainVictimPosition();
+  }
 
-  // Add event listener for keydown event
-  document.addEventListener('keydown', (event) => {
-    if (event.key === "ArrowLeft" && victimX > 0) { 
-      victimX -= stepSize;
-    } else if (event.key === "ArrowRight" && victimX < windowWidth - mainVictim.clientWidth) { 
-      victimX += stepSize;
-    } 
-    updateMainVictimPosition();
-  });
-
-  // Function to create flags at random intervals
-  const createRandomFlags = () => {
-    const randomTime = Math.floor(Math.random() * 1000) + 1000; 
-
+  createRandomFlags() {
+    const randomTime = Math.floor(Math.random() * 1000) + 1000;
     setTimeout(() => {
       if (Math.random() < 0.5) {
-        createFlag(document.querySelector('.red-flags'), 'red');
+        this.createFlag(this.redFlagContainer, 'red');
       } else {
-        createFlag(document.querySelector('.green-flags'), 'green');
+        this.createFlag(this.greenFlagContainer, 'green');
       }
-
-      createRandomFlags();
+      this.createRandomFlags();
     }, randomTime);
-  };
+  }
 
-  // Call the initial flag creation function
-  createRandomFlags();
-});
+}
+
+const game = new Game();
+
+
+
