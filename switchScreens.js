3e3f757e789921f47a11d243a548f5e544111214
Sena Aydin <senaaydin484@gmail.com>
2023-11-06 15:11:08 +0100

    remove src folder for deployment

--- switchScreens.js
+++ switchScreens.js
@@ -1,5 +1,5 @@
 function switchScreens () {
-   const button = document.getElementById("start-button")
+    const button = document.getElementById("start-button")
     document.getElementById("start-screen").style.display = "none";
     document.getElementById("game-screen").style.display = "block"
     document.querySelector("body").style.background = "rgb(238,174,202) radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)";
